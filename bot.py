5ac9ea9ad9b121d8dc3931bc9d3d33359c18853a
menakakarichiyappakumar <70016091+menakakarichiyappakumar@users.noreply.github.com>
2021-03-24 18:34:52 +0530

    Slack bot created

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
 import search
+import pydocs
 import slack
 import os
-import string
-import time
 
 from pathlib import Path
 from flask import Flask, request, Response
-from slackeventsapi import SlackEventAdapter
-from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,18 +13,23 @@ slack_secret = os.environ['SIGNING_SECRET']
 
 app = Flask(__name__)
 
-slack_event_adapter = SlackEventAdapter(slack_secret, '/slack/events', app)
 client = slack.WebClient(token= slack_token)
 
 @app.route("/search_string", methods=['POST'])
 def search_string ():
     data = request.form
-    user_id = data.get('user_id')
     channel_id = data.get('channel_id')
     text = search.main(data.get('text'))
+    client.chat_postMessage(channel=channel_id, text=text)
+    return Response(), 200
+
+@app.route("/search_pydocs", methods=['POST'])
+def search_pydocs ():
+    data = request.form
+    channel_id = data.get('channel_id')
+    text = pydocs.main(data.get('text'))
     client.chat_postMessage(
             channel=channel_id, text=text)
-
     return Response(), 200
 
 if __name__ == "__main__":
